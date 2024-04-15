2fe879550ce28f02deddbca0ba641eb08856ed27
unknown <vietanh912000@gmail.com>
2024-04-15 12:15:04 +0700

    update code

---
+++ WebApp/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApp.Areas.Admin.Service;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    public class BannerController : Controller
+    {
+        private BannerService bannerService;
+
+        public BannerController(IOptions<AppSettings> settings)
+        {
+            bannerService = new BannerService(settings);
+        }
+        // GET: BannerController
+        public ActionResult Index(int pageIndex,int pageSize)
+        {
+            var model = bannerService.GetAllBanner(pageIndex, pageSize);
+            return View(model);
+        }
+
+        // GET: BannerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BannerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BannerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BannerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BannerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BannerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BannerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApp/Areas/Admin/Service/BannerService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using PagedList;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin.Service
+{
+    public class BannerService
+    {
+
+        private WebContext _webContext;
+        public BannerService(IOptions<AppSettings> settings)
+        {
+           _webContext = new WebContext(settings);
+        }
+        
+        /// <summary>
+        /// get all banner paging
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public IEnumerable<Banner> GetAllBanner(int pageIndex = 1,int pageSize = 10)
+        {
+            var list = _webContext.banners.OrderBy(x => x.BannerId).ToList().ToPagedList(pageIndex, pageSize);
+            return list;
+        }
+
+    }
+}
